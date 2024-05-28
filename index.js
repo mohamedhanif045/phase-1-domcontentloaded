f0414045f65d47d6c97b103a4387e5be0bff4048
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-28 13:18:34 +0300

    phase-1-domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// Your code goes here!
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    // Select the paragraph element with id "text"
+    const paragraph = document.getElementById('text');
+    
+    // Change the text content of the paragraph
+    paragraph.textContent = "This is really cool!";
+});
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
